6aee293015e42163650842ccbae37efb1912b312
Rob Villeneuve <viller@deletedtheory.com>
2026-07-14 15:40:37 -0400

    Release v0.15.260714.1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'onik/lens-wp',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '74c18694f71b75796b292ab50e8f7f0e70d175c9',
+        'reference' => '4c2ea7419b69e7920b874b60b3bc5e4dabcb5879',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'onik/lens-wp' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '74c18694f71b75796b292ab50e8f7f0e70d175c9',
+            'reference' => '4c2ea7419b69e7920b874b60b3bc5e4dabcb5879',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
