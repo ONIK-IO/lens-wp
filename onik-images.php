bb3be472dfdca4a8c836a261df102f3e31cc3c80
Rob Villeneuve <viller@deletedtheory.com>
2026-07-14 22:09:15 -0400

    Release v0.15.260714.3

--- onik-images.php
+++ onik-images.php
@@ -3,7 +3,7 @@
  * Plugin Name:       ONIK Lens
  * Plugin URI:        https://onik.io/wp/lens
  * Description:       ONIK Lens automatically optimizes images and YouTube videos. See Settings -> ONIK Lens for configuration.
- * Version:           0.15.260714.2
+ * Version:           0.15.260714.3
  * Author:            ONIK
  * Author URI:        https://onik.io/
  * Requires at least: 6.0
@@ -11,7 +11,7 @@
  * Requires PHP:      8.1
  */
 
-define('ONIK_IMAGES_VERSION', '0.15.260714.2');
+define('ONIK_IMAGES_VERSION', '0.15.260714.3');
 
 require_once __DIR__ . '/vendor/autoload.php';
 
